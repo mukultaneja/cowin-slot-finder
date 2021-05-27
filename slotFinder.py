a6febbe068e7e1908f64879653ba18060e6e752f
Mukul Taneja <mtaneja@vmware.com>
2021-05-28 01:57:20 +0530

    Adding notification for windows system

--- slotFinder.py
+++ slotFinder.py
@@ -1,3 +1,17 @@
+'''
+description
+    - A continuous & multiprocessing python script to find
+      an available slot for covid vaccine. As soon as a slot
+      gets available based on the given search criteria this
+      script starts beeping and notifying/dumping relevant
+      information inside `slots-finder.log` in the executing
+      directory.
+usage
+    - python3 slotFinder.py slotInfo.json
+author
+    - Mukul Taneja
+'''
+
 import os
 import json
 import time
@@ -15,24 +29,32 @@ logging.basicConfig(level=logging.INFO,
 
 LOCK = Lock()
 
+
 def notifySlot(communicationType):
     if communicationType == 'system':
         message, title = 'Co-Win Slot Found', 'Success'
+        beepy.beep(sound="ping")
         if platform.system().lower() == 'darwin':
             command = f'''osascript -e 'display notification "{message}" with title "{title}"' '''
-        beepy.beep(sound="ping")
-        os.system(command)
+            os.system(command)
+
+        if platform.system().lower() == 'windows':
+            from plyer import notification
+            notification.notify(title=title, message=message, timeout=60)
 
 
 def dumpIntoFile(center, session):
-    msg = 'Found a slot near you..\n'
+    msg = '====== Found a slot near you.. ====== \n'
     msg += 'Name = {0}\n'.format(center.get('name'))
     msg += 'Address = {0}\n'.format(center.get('address'))
     msg += 'Date = {0}\n'.format(session.get('date'))
     msg += 'Available Capacity = {0}\n'.format(session.get('available_capacity'))
     msg += 'Vaccine = {0}\n'.format(session.get('vaccine'))
     msg += 'Fee Type = {0}\n'.format(center.get('fee_type'))
-    msg += 'Slots = {0}\n\n'.format(session.get('slots'))
+    msg += 'Slots = {0}\n'.format(session.get('slots'))
+    msg += 'Pincode = {0}\n'.format(center.get('pincode'))
+    msg += 'District Name = {0}\n'.format(center.get('district_name'))
+    msg += '==================================== \n\n'
 
     with LOCK:
         with open('slots-finder.log', 'a') as slotFinderLogs:
@@ -45,10 +67,13 @@ def isSlotAvailable(response, searchCriteria):
         for session in sessions:
             if session.get('available_capacity') > 0:
                 ageLimits = searchCriteria.get('minAgeLimit').split(',')
+                ageLimits = [ageLimit.strip() for ageLimit in ageLimits]
                 if str(session.get('min_age_limit')) in ageLimits:
                     vaccines = searchCriteria.get('vaccineName').split(',')
+                    vaccines = [vaccine.strip() for vaccine in vaccines]
                     if session.get('vaccine') in vaccines:
                         feeTypes = searchCriteria.get('feeType').split(',')
+                        feeTypes = [feeType.strip() for feeType in feeTypes]
                         if center.get('fee_type') in feeTypes:
                             msg = "{0}, {1}, {2} {3} {4} {5}"
                             msg = msg.format(session.get('available_capacity'),
@@ -64,12 +89,10 @@ def isSlotAvailable(response, searchCriteria):
     return False
 
 
-def getSlotInformation(dataPoint, searchBy, searchCriteria, communicationType):
-    host = "https://cdn-api.co-vin.in"
-    endpoint = "api/v2/appointment/sessions/public/calendarByDistrict"
-    if searchBy == "pincode":
-        endpoint = "api/v2/appointment/sessions/public/calendarByPin"
-    url = "{0}/{1}".format(host, endpoint)
+def getSlotInformation(dataPoint, searchCriteria, communicationType):
+    url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public"
+    endpoint = "calendarByPin" if dataPoint.get("pincode") else "calendarByDistrict"
+    url = "{0}/{1}".format(url, endpoint)
 
     if not dataPoint.get("date", None):
         currentHour = datetime.now().hour
@@ -98,6 +121,7 @@ def getSlotInformation(dataPoint, searchBy, searchCriteria, communicationType):
 def main(inputData):
     numsOfSentRequestsPerMin = timeCounter = minutes = 0
     dataPoints = inputData.get("dataPoints", None)
+
     if not dataPoints:
         logging.info("No data points to poll")
         return
@@ -105,26 +129,26 @@ def main(inputData):
     numOfDataPoints = len(dataPoints)
     numsOfRequestsPerMin = 20 # Arogya setu app allows 100 requests per 5 mins
     timeToSleep = numOfDataPoints * (60 // numsOfRequestsPerMin)
-    searchBy = inputData.get("searchBy", "district")
     searchCriteria = inputData.get("searchCriteria")
     communicationType =  inputData.get("communicationType", 'system')
 
     while True:
         if timeCounter % 60 == 0:
-            msg = "Number of sent requests {0} in {1} min(s)"
+            msg = "======= Number of sent requests {0} in {1} min(s) ======="
             logging.info(msg.format(numsOfSentRequestsPerMin, minutes))
             minutes += 1
 
-        searchProgresses = list()
+        searchProcesses = list()
         for dataPoint in dataPoints:
             numsOfSentRequestsPerMin += 1
-            process = Process(target=getSlotInformation, args=(
-                dataPoint, searchBy, searchCriteria, communicationType))
+            process = Process(target=getSlotInformation,
+                              args=(dataPoint, searchCriteria, communicationType))
             process.start()
-            searchProgresses.append(process)
+            searchProcesses.append(process)
 
-        for process in searchProgresses:
+        for process in searchProcesses:
             process.join()
+
         logging.info("Sleeping for {0} sec(s)...".format(timeToSleep))
         time.sleep(timeToSleep)
         timeCounter += timeToSleep
@@ -132,7 +156,7 @@ def main(inputData):
 
 def parseCmd():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', action='store', dest='input', help='Input JSON file')
+    parser.add_argument('input', type=str, help='Input JSON file')
 
     return parser.parse_args()
 
@@ -140,7 +164,7 @@ def parseCmd():
 if __name__ == "__main__":
     args = parseCmd()
     if not os.path.exists(args.input):
-        raise FileNotFoundError("Missing Input JSON file")
+        raise FileNotFoundError("No file found named {0}".format(args.input))
 
     logging.info("Starting Slot-Finder with {0}".format(args))
     inputData = dict()
